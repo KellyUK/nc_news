4682783621b3697bd04cb1cf289ad153f7d89a0c
Kelly Davidson <kellyadavidsonuk@gmail.com>
2019-06-24 16:20:47 +0100

    added cors

--- listen.js
+++ listen.js
@@ -1,4 +1,16 @@
-const app = require('./app');
+const app = require("./app");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.get("/products/:id", function(req, res, next) {
+  res.json({ msg: "This is CORS-enabled for all origins!" });
+});
+
+app.listen(80, function() {
+  console.log("CORS-enabled web server listening on port 80");
+});
 
 const { PORT = 9090 } = process.env;
 
