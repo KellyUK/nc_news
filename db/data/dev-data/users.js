a577e20522891557ebabc086dc20b457be9b5c4e
Kelly Davidson <kellyadavidsonuk@gmail.com>
2019-06-30 15:45:19 +0100

    updated mr men images with current img links

--- db/data/dev-data/users.js
+++ db/data/dev-data/users.js
@@ -3,12 +3,13 @@ module.exports = [
     username: "tickle122",
     name: "Tom Tickle",
     avatar_url:
-      "https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg"
+      "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiJ9oyvuJHjAhXwDmMBHaD-AyEQjRx6BAgBEAU&url=https%3A%2F%2Fmrmen.fandom.com%2Fwiki%2FMr._Tickle&psig=AOvVaw2z5kTp5C2F5yo_jRdml86D&ust=1561992139171188"
   },
   {
     username: "grumpy19",
     name: "Paul Grump",
-    avatar_url: "https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg"
+    avatar_url:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.jasondale.co.uk%2Fwp-content%2Fuploads%2F2017%2F02%2Fcharacterpic-square-mrgrumpy.jpg&imgrefurl=https%3A%2F%2Fwww.jasondale.co.uk%2Flifestyle%2Fgoodbye-to-mr-grumpy.htm&docid=KYdJmfM044JxqM&tbnid=kFSA8d1azO8AEM%3A&vet=10ahUKEwiJ4MXFuJHjAhUOnxQKHfVmAfwQMwhbKAwwDA..i&w=633&h=491&bih=787&biw=1149&q=mr.%20grumpy&ved=0ahUKEwiJ4MXFuJHjAhUOnxQKHfVmAfwQMwhbKAwwDA&iact=mrc&uact=8"
   },
   {
     username: "happyamy2016",
@@ -19,13 +20,14 @@ module.exports = [
   {
     username: "cooljmessy",
     name: "Peter Messy",
-    avatar_url: "https://i.imgur.com/WfX0Neu.jpg"
+    avatar_url:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fmrmen%2Fimages%2F1%2F17%2FMr._Messy_show.png%2Frevision%2Flatest%2Fscale-to-width-down%2F180%3Fcb%3D20180723214026&imgrefurl=https%3A%2F%2Fmrmen.fandom.com%2Fwiki%2FMr._Messy&docid=Rph9PxihOO0a6M&tbnid=68V-3N4eohPoFM%3A&vet=10ahUKEwj90-XiuJHjAhVPxYUKHRZUBAYQMwhRKAUwBQ..i&w=180&h=135&bih=787&biw=1149&q=mr.%20messy&ved=0ahUKEwj90-XiuJHjAhVPxYUKHRZUBAYQMwhRKAUwBQ&iact=mrc&uact=8"
   },
   {
     username: "weegembump",
     name: "Gemma Bump",
     avatar_url:
-      "https://www.upandrunning.co.uk/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/r/mr-bump.jpg"
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fmrmen%2Fimages%2F7%2F7e%2FMrMen-Bump.png%2Frevision%2Flatest%2Fscale-to-width-down%2F250%3Fcb%3D20180123225553&imgrefurl=https%3A%2F%2Fmrmen.fandom.com%2Fwiki%2FMr._Bump&docid=201IJH7Zfz6TjM&tbnid=Uaa-MeCxYzNtFM%3A&vet=10ahUKEwi6hoj_t5HjAhXF0eAKHUSwDSQQMwhiKAAwAA..i&w=250&h=217&bih=787&biw=1149&q=mr.%20bump%20image&ved=0ahUKEwi6hoj_t5HjAhXF0eAKHUSwDSQQMwhiKAAwAA&iact=mrc&uact=8"
   },
   {
     username: "jessjelly",
